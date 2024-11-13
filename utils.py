9d047c12b3c727a34e8583f159b5c8e40e09d721
jq1836 <jordanquahshaoxuan@gmail.com>
2024-11-13 16:43:57 +0800

    Add COMET to eval.py

--- utils.py
+++ utils.py
@@ -1,15 +1,24 @@
-import evaluate
+from evaluate import load
 import torch
 
-sacrebleu = evaluate.load("sacrebleu")
+sacrebleu = load("sacrebleu")
+comet = load("metrics/comet/comet.py")
 
+'''
+Evaluation utils
+'''
 def compute_single_bleu_score(pred, lab):
     return sacrebleu.compute(predictions=[pred], references=[[lab]])["score"]
 
 def compute_bleu_score(pred, lab):
     return sacrebleu.compute(predictions=pred, references=lab)["score"]
 
+def compute_comet_score(src, pred, lab):
+    return comet.compute(predictions=pred, references=lab, sources=src)["scores"]
 
+'''
+File utils
+'''
 def write_file(file_path, content):
     with open(file_path, "w", encoding="utf-8") as file:
         file.writelines(content)
@@ -19,6 +28,9 @@ def read_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file: 
         return file.read().splitlines()
 
+'''
+Other utils
+'''
 def get_device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
